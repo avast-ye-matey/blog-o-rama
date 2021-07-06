633d66e9159a48ebcf2b63b6489c8e2f0e4a4f18
avast-ye-matey <immdcd@hotmail.com>
2021-07-06 16:56:12 -0700

    added text form for adding blog posts

--- script.js
+++ script.js
@@ -139,3 +139,14 @@ document.getElementById("addButton").addEventListener("click", function() {
     /* container.body.appendParent(btn); */
     /* btn.insertAdjacentHTML( 'afterend', '<hr id="appendHere">' ); */
 });
+
+
+
+
+
+{/* <form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname">
+</form> */}
